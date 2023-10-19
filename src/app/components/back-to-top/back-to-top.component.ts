75863ea9758ed0b767318ee69943b2b28cdd9407
Yash15o2 <yashthakur15022002@gmail.com>
2023-10-19 06:56:17 +0530

    Added back to top functionality

--- src/app/components/back-to-top/back-to-top.component.ts
+++ src/app/components/back-to-top/back-to-top.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-back-to-top',
   templateUrl: './back-to-top.component.html',
-  styleUrls: ['./back-to-top.component.css']
+  styleUrls: ['./back-to-top.component.css'],
 })
 export class BackToTopComponent {
+  showScrollToTopButton = false;
 
+  constructor() {}
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
